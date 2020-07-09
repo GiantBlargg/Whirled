0edfd5c47014e4070cb9515af83dc4688356cc31
Daniel Doran <ddoran3@gmail.com>
2020-07-09 02:15:36 -0600

    terrain cutouts

--- scripts/LR2/TDF.cs
+++ scripts/LR2/TDF.cs
@@ -45,21 +45,6 @@ public class TDF : MeshInstance3D {
 
 		file.Seek(0x20);
 
-		var indices = new int[CHUNK_WIDTH * CHUNK_WIDTH * 6];
-
-		for (var z = 0; z < CHUNK_WIDTH; z++) {
-			for (var x = 0; x < CHUNK_WIDTH; x++) {
-				var index = (z * CHUNK_WIDTH + x) * 6;
-				var base_vertex = (z * VERTEX_CHUNK + x);
-				indices[index] = base_vertex;
-				indices[index + 1] = base_vertex + 1;
-				indices[index + 2] = base_vertex + VERTEX_CHUNK;
-				indices[index + 3] = base_vertex + VERTEX_CHUNK;
-				indices[index + 4] = base_vertex + 1;
-				indices[index + 5] = base_vertex + 1 + VERTEX_CHUNK;
-			}
-		}
-
 		var mesh = new ArrayMesh();
 
 		for (int cx = 0; cx < NUM_CHUNKS; cx++) {
@@ -67,6 +52,7 @@ public class TDF : MeshInstance3D {
 
 				var vertices = new Vector3[VERTEX_CHUNK * VERTEX_CHUNK];
 				var normals = new Vector3[VERTEX_CHUNK * VERTEX_CHUNK];
+				var cutout = new bool[VERTEX_CHUNK * VERTEX_CHUNK];
 
 				for (int sz = 0; sz < VERTEX_CHUNK; sz++) {
 					for (int sx = 0; sx < VERTEX_CHUNK; sx++) {
@@ -77,9 +63,30 @@ public class TDF : MeshInstance3D {
 								cz * CHUNK_WIDTH + sz - CHUNK_WIDTH * NUM_CHUNKS / 2);
 						normals[index] = new Vector3((sbyte)file.Get8(), (sbyte)file.Get8(), (sbyte)file.Get8());
 
-						file.Seek(file.GetPosition() + 3);
+						cutout[index] = (file.Get8() & 1) == 1;
+
+						file.Seek(file.GetPosition() + 2);
 					}
 				}
+
+				var indices = new int[CHUNK_WIDTH * CHUNK_WIDTH * 6];
+
+				for (var z = 0; z < CHUNK_WIDTH; z++) {
+					for (var x = 0; x < CHUNK_WIDTH; x++) {
+						var index = (z * CHUNK_WIDTH + x) * 6;
+						var base_vertex = (z * VERTEX_CHUNK + x);
+
+						if (!cutout[base_vertex + 1 + VERTEX_CHUNK]) {
+							indices[index] = base_vertex;
+							indices[index + 1] = base_vertex + 1;
+							indices[index + 2] = base_vertex + VERTEX_CHUNK;
+							indices[index + 3] = base_vertex + VERTEX_CHUNK;
+							indices[index + 4] = base_vertex + 1;
+							indices[index + 5] = base_vertex + 1 + VERTEX_CHUNK;
+						}
+					}
+				}
+
 				var array = new Godot.Collections.Array();
 				array.Resize((int)ArrayMesh.ArrayType.Max);
 				array[(int)ArrayMesh.ArrayType.Vertex] = vertices;
