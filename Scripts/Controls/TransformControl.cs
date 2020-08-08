5845f05044c6ad613c21193ab502e8413134a149
Daniel Doran <ddoran3@gmail.com>
2020-08-07 21:45:55 -0600

    TransformControl use Vector3Control

--- Scripts/Controls/TransformControl.cs
+++ Scripts/Controls/TransformControl.cs
@@ -5,31 +5,29 @@ namespace Controls {
 		static float RadToDeg = 180 / Mathf.Pi;
 		static float DegToRad = Mathf.Pi / 180;
 
-		TransformControlSubSection translate, rotation, scale;
+		Vector3Control translate, rotation, scale;
 
 		bool AllowScale;
 
-		public TransformControl(bool _AllowScale = false) {
-			AllowScale = _AllowScale;
+		public TransformControl(bool allowScale = false) {
+			AllowScale = allowScale;
 
 			SizeFlagsHorizontal = (int)Control.SizeFlags.ExpandFill;
 
-			var transLabel = new Label();
-			transLabel.Text = "Translation";
-			AddChild(transLabel);
-			translate = new TransformControlSubSection(this);
-
-			var rotLabel = new Label();
-			rotLabel.Text = "Rotation";
-			AddChild(rotLabel);
-			rotation = new TransformControlSubSection(this);
+			translate = AddComponent("Translation");
+			rotation = AddComponent("Rotation");
 
 			if (AllowScale) {
-				var scaleLable = new Label();
-				scaleLable.Text = "Scale";
-				AddChild(scaleLable);
-				scale = new TransformControlSubSection(this);
+				scale = AddComponent("Scale");
 			}
+		}
+
+		Vector3Control AddComponent(string name) {
+			var component = new Vector3Control();
+			component.Name = name;
+			component.ValueSet += h => ValueSet(h);
+			AddChild(component);
+			return component;
 		}
 
 		public event ValueSet ValueSet;
@@ -39,68 +37,19 @@ namespace Controls {
 
 		public Transform Value {
 			get {
-				var basis = new Basis(rotation.value * DegToRad);
+				var basis = new Basis(rotation.Value * DegToRad);
 				if (AllowScale) {
-					basis = basis.Scaled(scale.value);
+					basis = basis.Scaled(scale.Value);
 				}
-				return new Transform(basis, translate.value);
+				return new Transform(basis, translate.Value);
 			}
 			set {
-				translate.Update(value.origin);
-				rotation.Update(value.basis.GetEuler() * RadToDeg);
+				translate.Value = value.origin;
+				rotation.Value = value.basis.GetEuler() * RadToDeg;
 				if (AllowScale) {
-					scale.Update(value.basis.Scale);
+					scale.Value = value.basis.Scale;
 				}
 			}
-		}
-	}
-
-	class TransformControlSubSection : HBoxContainer {
-		LineEdit x, y, z;
-		public TransformControlSubSection(TransformControl parent) {
-			x = CreateComponent(parent);
-			y = CreateComponent(parent);
-			z = CreateComponent(parent);
-
-			parent.AddChild(this);
-		}
-
-		LineEdit CreateComponent(TransformControl parent) {
-			var component = new LineEdit();
-
-			component.SizeFlagsHorizontal = (int)Control.SizeFlags.ExpandFill;
-
-			component.Connect("text_entered", parent, nameof(TransformControl.SetTransform));
-			component.Connect("focus_exited", parent, nameof(TransformControl.SetTransform));
-
-			AddChild(component);
-			return component;
-		}
-
-		float Parse(string v) {
-			float f;
-			if (float.TryParse(v, out f)) return f;
-			return 0;
-		}
-
-		public Vector3 value {
-			get {
-				return new Vector3(
-					Parse(x.Text),
-					Parse(y.Text),
-					Parse(z.Text)
-				);
-			}
-			set { Update(value); }
-		}
-
-		public void Update(Vector3 vector) {
-			UpdateComponent(x, vector.x);
-			UpdateComponent(y, vector.y);
-			UpdateComponent(z, vector.z);
-		}
-		static void UpdateComponent(LineEdit line, float value) {
-			line.Text = value.ToString();
 		}
 	}
 }
